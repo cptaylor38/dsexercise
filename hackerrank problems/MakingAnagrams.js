c776cc0343d559655db85b5ab75558eec4d3a64a
Corbin Taylor <cptaylor38@yahoo.com>
2020-12-07 10:05:41 -0500

    1 test case, rethinking logic

---
+++ hackerrank problems/MakingAnagrams.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the makeAnagram function below.
+function makeAnagram(a, b) {
+    if(a === b) return a.length;
+    
+    if(a.length > b.length){
+        for(let char of a){
+            if(b.includes(char)){
+                b.split(' ').splice(b.indexOf(char)).join('')
+            }
+        }
+    }
+    return b.length;
+
+    //1 test case. 
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine();
+
+    const b = readLine();
+
+    const res = makeAnagram(a, b);
+
+    ws.write(res + '\n');
+
+    ws.end();
+}
