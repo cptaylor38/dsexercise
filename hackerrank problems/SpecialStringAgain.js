f8643cafa1414f946cecc063a97dc2bd0b668462
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-14 16:10:36 -0500

    spending 6 hours on a solution that still fails is not good. need more practice.

--- hackerrank problems/SpecialStringAgain.js
+++ hackerrank problems/SpecialStringAgain.js
@@ -6,22 +6,41 @@ let url = 'https://www.hackerrank.com/challenges/special-palindrome-again/proble
 // All characters except the middle one are the same, e.g. aadaa.
 // A special substring is any substring of a string which meets one of those criteria. Given a string, determine how many special substrings can be formed from it.
 
-let string = 'abcbaba';
+let string = 'abcbabaaejejjfiefeiveivieivjeivajiejiejiajeiajeijaeijfjeee';
 let stringLength = string.length;
 
-function substrCount(n, s) {
-    let count = n;
-    for(let i = 0; i < s.length; i++){
-        for(let j = i + 1; j < s.length; j++){
-             if(s[j] === s[j - 1]) count++;
-             else {
-                 if(s[j + 1] === s[i]) count++;
-                 else break;
-             }
-        }
-    }
-    return count;
- }
+// function substrCount(n, s) {
+//     let count = n;
+//     for(let i = 0; i < s.length; i++){
+//         for(let j = i + 1; j < s.length; j++){
+//              if(s[j] === s[j - 1]) count++;
+//              else {
+//                  if(s[j + 1] === s[i]) count++;
+//                  else break;
+//              }
+//         }
+//     }
+//     return count;
+//  }
+
+//copied working solution to study
+// function substrCount(n, s) {
+//     let count = n;
+//      let currentSequence = s[0];
+//      for(let i = 1; i < n; i++){
+//          if (s[i] === s[i-1]){
+//              count+= currentSequence.length;
+//              currentSequence = currentSequence + s[i];
+//          } else {
+//              for(let j = 0; j < currentSequence.length; j++){
+//                  if(s[i + 1 + j] === currentSequence[0]){ count++; }
+//                  else{ break; }
+//              }
+//              currentSequence = s[i];
+//          }
+//      }
+//  return count;
+//  }
 
 // function substrCount(n, s) {
 //     let knownSubstrings = 0;
