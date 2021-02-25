36d563c3edd706217a11bccbb4d2d2f6216006e0
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-24 23:41:59 -0500

    just reworking logic

--- aexpert/Non-ConstructibleChange.js
+++ aexpert/Non-ConstructibleChange.js
@@ -1,33 +1,11 @@
 function nonConstructibleChange(coins) {
-    // Write your code here.
-      coins.sort();
-      let missingChange;
-      if(coins.length === 0) return 1;
-      for(let i = 0; i < coins.length; i++){
-          if(coins[i - 1] !== coins[i] - 1) missingChange = coins[i] - 1;
-          if(missingChange){
-              let sum = 0;
-              for(let j = 0; j < i; j++){
-                  if(sum === missingChange) break;
-                  else sum += coins[j];
-              }
-              if(sum !== missingChange) return missingChange;
-          }
-      }
-      
-      if(!missingChange){
-          let nextNum = coins[coins.length - 1] + 1;
-          let sum = 0;
-          while(!missingChange){
-              for(let c = 0; c < coins.length; c++){
-                  if(sum === nextNum) nextNum++;
-                  else sum += coins[c];
-              }
-              if(sum !== nextNum){
-                  missingChange = nextNum;
-              }
-          }
-      }
-      
-      return missingChange;
+    coins.sort();
+    //sort, then find a missing number, check numbers before missing number to see if they total the missing number
+    //if not
+    //choose one number above the last number in the array.
+
+    //mapping out logic
   }
+
+//1, 3, 4, 5, 6, 7, 8, 9 - Expected result 2.
+nonConstructibleChange([1, 2, 4, 5])
